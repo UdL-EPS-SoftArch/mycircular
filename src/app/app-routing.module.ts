022f3ae088658e17baa99c0251ccffec49c1d39a
Nino Gosto <ninogosto11@gmail.com>
2022-11-17 18:18:21 +0100

    Creating review component and adding review link to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,16 +8,18 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
+import { ReviewComponent } from './review/review.component';
 
 const routes: Routes = [
-  { path: 'users/create', component: UserRegisterComponent},
-  { path: 'users/:id/delete', component: UserDeleteComponent, canActivate: [LoggedInGuard]},
-  { path: 'users/:id/edit', component: UserEditComponent, canActivate: [LoggedInGuard]},
-  { path: 'users/:id', component: UserDetailComponent, canActivate: [LoggedInGuard]},
-  { path: 'users', component: UserListComponent, canActivate: [LoggedInGuard]},
-  { path: 'about', component: AboutComponent},
-  { path: '404', component: NotFoundComponent},
-  { path: '', redirectTo: 'about', pathMatch: 'full'},
+  { path: 'users/create', component: UserRegisterComponent },
+  { path: 'users/:id/delete', component: UserDeleteComponent, canActivate: [LoggedInGuard] },
+  { path: 'users/:id/edit', component: UserEditComponent, canActivate: [LoggedInGuard] },
+  { path: 'users/:id', component: UserDetailComponent, canActivate: [LoggedInGuard] },
+  { path: 'users', component: UserListComponent, canActivate: [LoggedInGuard] },
+  { path: 'about', component: AboutComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'reviews', component: ReviewComponent, canActivate: [LoggedInGuard] }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {HttpErrorInterceptor} from './error-handler/http-error-interceptor';
 import {AuthenticationBasicService} from './login-basic/authentication-basic.service';
 import {LoggedInGuard} from './login-basic/loggedin.guard';
 import {UserService} from './user/user.service';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import {UserService} from './user/user.service';
     UserRegisterComponent,
     UserEditComponent,
     UserDeleteComponent,
-    UserSearchComponent
+    UserSearchComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
