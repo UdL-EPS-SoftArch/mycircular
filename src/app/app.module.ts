03b052f69c5d8c2105c65d53a0436adafd67368e
SaraSerrano7 <sasecu@gmail.com>
2022-11-30 15:57:06 +0100

    Pushing for my mate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,8 @@ import {HttpErrorInterceptor} from './error-handler/http-error-interceptor';
 import {AuthenticationBasicService} from './login-basic/authentication-basic.service';
 import {LoggedInGuard} from './login-basic/loggedin.guard';
 import {UserService} from './user/user.service';
+//import { RequestsComponent } from './requests/requests.component';
+import { RequestsListComponent } from './requests/requests-list/requests-list.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,9 @@ import {UserService} from './user/user.service';
     UserRegisterComponent,
     UserEditComponent,
     UserDeleteComponent,
-    UserSearchComponent
+    UserSearchComponent,
+//    RequestsComponent,
+    RequestsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/requests/requests-list/requests-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RequestsListComponent } from './requests-list.component';
+
+describe('RequestsListComponent', () => {
+  let component: RequestsListComponent;
+  let fixture: ComponentFixture<RequestsListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RequestsListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RequestsListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
