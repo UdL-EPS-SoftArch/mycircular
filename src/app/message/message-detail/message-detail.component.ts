50437cc081d669079a35f36f27f080487654def3
jnoguero <jrndll2@gmail.com>
2022-12-12 18:04:50 +0100

    Create message-detail

--- src/app/message/message-detail/message-detail.component.ts
+++ src/app/message/message-detail/message-detail.component.ts
@@ -1,15 +1,28 @@
-import {Component, OnInit} from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MessageService} from "../message.service";
+import { Message} from "../message";
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-message-detail',
-  templateUrl:"./message-detail.component.html",
+  templateUrl: './message-detail.component.html'
 })
-export class MessageDetailComponent implements OnInit{
+export class MessageDetailComponent implements OnInit {
+  public message: Message = new Message();
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private messageService: MessageService,
+              private authenticationService: AuthenticationBasicService) {
   }
+
   ngOnInit(): void {
-    return;
+    const id = this.route.snapshot.paramMap.get('id');
+    this.messageService.getResource(id).subscribe(
+      message => {
+        this.message = message;
+      });
   }
+
 
 }
