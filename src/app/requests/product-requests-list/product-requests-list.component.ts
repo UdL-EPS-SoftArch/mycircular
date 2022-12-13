7056900483dbac32c09855f286ee12345423165a
SaraSerrano7 <sasecu@gmail.com>
2022-12-13 16:51:17 +0100

    product request list working fine. TO DO: search bar [WIP]

--- src/app/requests/product-requests-list/product-requests-list.component.ts
+++ src/app/requests/product-requests-list/product-requests-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ProductRequestService} from '../product-request.service';
+import {ProductRequest} from "../product-request";
+import {HttpMethod, PagedResourceCollection} from '@lagoshny/ngx-hateoas-client';
+import {AuthenticationBasicService} from "../../login-basic/authentication-basic.service";
 
 @Component({
   selector: 'app-product-requests-list',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductRequestsListComponent implements OnInit {
 
-  constructor() { }
+  public productRequests: ProductRequest[] = [];
+  public pageSize: number = 5;
+  public page: number = 1;
+  public totalProdRequests: number = 0;
+  public currentUsername: string = this.authenticationBasicService.getCurrentUser().id
+  public userParam : string = this.getUsernameParam()
+
+  constructor(public router: Router,
+              private productRequestService: ProductRequestService,
+              private authenticationBasicService: AuthenticationBasicService,
+  ) {
+  }
+
+  getUsernameParam(): string {
+    return "?username=".concat(this.currentUsername)
+  }
 
   ngOnInit(): void {
+
+    console.log(this.userParam)
+    this.productRequestService.customQuery(HttpMethod.GET, this.userParam)
+      .subscribe(
+        (productRequests: ProductRequest[]) => {
+          for(let productRequest of productRequests) {
+            console.log(productRequest)
+          }
+          this.productRequests = productRequests
+          console.log("i got it")
+          console.log(typeof productRequests)
+          console.log(productRequests[0].name)
+        }
+      )
+    console.log(this.productRequests)
   }
+
+  //todo: corregir la paginacion
+  changePage(): void {
+    this.productRequestService.getPage({
+      pageParams: {page: this.page - 1, size: this.pageSize},
+      sort: {name: 'ASC'}
+    }).subscribe(
+      (page: PagedResourceCollection<ProductRequest>) => {
+        this.productRequests = page.resources;
+        console.log(this.productRequests)
+      }
+    )
+  };
+
+
+  detail(productRequests: ProductRequest): void {
+    this.router.navigate(['prodRequests', productRequests.id]);
+  }
+
 
 }
