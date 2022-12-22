e34a27efa21a34460b2364b504edd1b4c3d48446
Marc Farran <mfarran01@gmail.com>
2022-12-22 17:56:14 +0100

    Adding more time

--- e2e/steps/transaction.ts
+++ e2e/steps/transaction.ts
@@ -18,7 +18,7 @@ Then ('It creates an announcment',()=>{
       "name": "portatil",
       "description": "Nuevo y reluciente, listo para usar"
     });
-    cy.wait(500);
+    cy.wait(1500);
 });
 
 Then('It creates a transaction',()=>{
@@ -28,7 +28,7 @@ Then('It creates a transaction',()=>{
       "buyer": "/users/demo",
       "price" : 12
   });
-    cy.wait(500);
+    cy.wait(1000);
 });
 
 Then ('I edit the price with the new value {string}',(price)=>{
